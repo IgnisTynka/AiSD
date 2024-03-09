5157105af973f3de3f644b3bf87b8f8032569616
IgnisTynka <marta.glowacka263@gmail.com>
2024-03-09 21:52:36 +0100

    Shell Sort

--- src/selection_sort.cpp
+++ src/selection_sort.cpp
@@ -2,23 +2,23 @@
 #include <memory>
 
 std::vector<int> selection_sort(std::vector<int> array) {
-    size_t  i, j, min_idx;
+    size_t  i, j, min_index;
   
     for (i = 0; i < array.size() - 1; i++) { 
-        min_idx = i; 
+        min_index = i; 
 
         // Find the minimum element in the unsorted array
         for (j = i + 1; j < array.size(); j++) { 
 
             /* If the current element is smaller than the minimum element
-            found so far change the min_idx */ 
-            if (array[j] < array[min_idx]) 
-                min_idx = j; 
+            found so far change the min_index */ 
+            if (array[j] < array[min_index]) 
+                min_index = j; 
         } 
   
         /* Swap the found minimum element with the i-th element */
-        if (min_idx != i) 
-            std::swap(array[i], array[min_idx]);
+        if (min_index != i) 
+            std::swap(array[i], array[min_index]);
     } 
     return array;
 }
--- src/shell_sort.cpp
+++ src/shell_sort.cpp
@@ -1,6 +1,24 @@
 #include "sort.h"
 
 std::vector<int> shell_sort(std::vector<int> array) {
-    //Marta
+    int temp;
+    size_t gap, i, j;
+
+    for (gap = array.size()/2; gap > 0; gap /= 2) 
+    { 
+        
+        for (i = gap; i < array.size(); i += 1) 
+        { 
+            int temp = array[i]; 
+  
+            /* shift earlier elements up until 
+            the correct location for temp (array[i]) is found */ 
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap) 
+                array[j] = array[j - gap]; 
+              
+            //  put temp (array[i]) in its correct location 
+            array[j] = temp; 
+        } 
+    } 
     return array;
 }
