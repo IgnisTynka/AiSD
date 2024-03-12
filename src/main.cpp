17caf98b2ae2b1000eaf89c725254cce6601feed
IgnisTynka <marta.glowacka263@gmail.com>
2024-03-12 15:40:34 +0100

    Benchmark random

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <memory>
-#include <fstream>
+#include <sstream>
 #include <string>
 #include "sort.h"
 
@@ -12,29 +12,8 @@ void print_array(std::vector<int> array){
     std::cout << std::endl;
 }
 
-// Function to read the file and return the array
-std::vector<int> read_file(std::string file_name){
-    std::ifstream file(file_name);
-    
-    if (file.bad()){
-        std::cout << "Error: file not found" << std::endl;
-        return std::vector<int>();
-    }
-
-    size_t size;
-    file >> size;
-
-    std::vector<int> array(size);
-    for(size_t i = 0; i < size && !file.eof(); i++){
-        file >> array[i];
-    }
-
-    file.close();
-    return array;
-}
-
 // Function to select the algorithm to sort the array 
-std::vector<int> select_algorithm(std::vector<int> array, int n){
+std::vector<int> sort_using_algorithm(std::vector<int> array, int n){
     std::vector<int> sorted_array;
     switch (n){
         case 1:
@@ -66,17 +45,28 @@ std::vector<int> select_algorithm(std::vector<int> array, int n){
 
 int main(int argc, char const *argv[]) {
 
-    if (argc < 3){
-        std::cout << "Error: missing arguments" << std::endl;
+    if (argc != 3 || std::string(argv[1]) != "--algorithm") {
+        std::cout << "Usage: ./executable --algorithm <algorithm_number>" << std::endl;
         return 1;
     }
 
-    std::string file_name = argv[1];
-    int algorithm_number = std::stoi(argv[2]);
-    auto array = read_file(file_name); 
+    // Get algorithm number from command line arguments
+    int algorithm_number;
+    std::istringstream(argv[2]) >> algorithm_number;
 
-    auto sorted_array = select_algorithm(array, algorithm_number);
-    print_array(sorted_array);
-    
+    // Read input data from standard input until the end of file (EOF)
+    std::vector<int> data;
+    int value;
+    while (std::cin >> value) {
+        data.push_back(value);
+    }
+
+    // Perform sorting using the specified algorithm
+    std::vector<int> sorted_data = sort_using_algorithm(data, algorithm_number);
+
+    // Print the sorted data (first 10 elements for simplicity)
+    std::cout << "Sorted data: ";
+    print_array(sorted_data);
+
     return 0;
 }  
