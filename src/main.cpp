5fc5428ebec05fb5fae740664181bd4ff77cf004
IgnisTynka <marta.glowacka263@gmail.com>
2024-03-14 20:25:14 +0100

    Main path file / main own array

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <fstream>
 #include <string>
 #include "sort.h"
 
-// Function to print the array
-void print_array(std::vector<int> array){
-    for (size_t i = 0; i < 10 && i < array.size(); i++){
-        std::cout << array[i] << " ";
+// Function to read the file (path algorithn_number main function)
+/*
+std::vector<int> read_file(std::string file_name){
+    std::ifstream file(file_name);
+    
+    if (file.bad()){
+        std::cout << "Error: file not found" << std::endl;
+        return std::vector<int>();
     }
-    std::cout << std::endl;
+
+    size_t size;
+    file >> size;
+
+    std::vector<int> array(size);
+    for(size_t i = 0; i < size && !file.eof(); i++){
+        file >> array[i];
+    }
+
+    file.close();
+    return array;
 }
+*/
 
 // Function to select the algorithm to sort the array 
 std::vector<int> sort_using_algorithm(std::vector<int> array, int n){
@@ -43,6 +59,15 @@ std::vector<int> sort_using_algorithm(std::vector<int> array, int n){
     return sorted_array;
 }
 
+// Function to print the array
+void print_array(std::vector<int> array){
+    for (size_t i = 0; i < 10 && i < array.size(); i++){
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// benchmark main function
 int main(int argc, char const *argv[]) {
 
     if (argc != 3 || std::string(argv[1]) != "--algorithm") {
@@ -70,3 +95,33 @@ int main(int argc, char const *argv[]) {
 
     return 0;
 }  
+
+// path algorithm_number main function
+/*
+int main(int argc, char const *argv[]) {
+
+    if (argc < 3){
+        std::cout << "Error: missing arguments" << std::endl;
+        return 1;
+    }
+
+    std::string file_name = argv[1];
+    int algorithm_number = std::stoi(argv[2]);
+    auto array = read_file(file_name); 
+
+    auto sorted_array =  sort_using_algorithm(array, algorithm_number);
+
+    std::cout << "Sorted data: ";
+    print_array(sorted_array);
+    
+    return 0;
+} 
+
+// own array main function
+void main(){
+    std::vector<int> array = {};
+    auto sorted_array =  sort_using_algorithm(array, 1);
+    std::cout << "Sorted data: ";
+    print_array(sorted_array);
+}
+*/
