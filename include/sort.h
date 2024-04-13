7433dd46152abb9d8915406dd71992e2406ec9ea
IgnisTynka <marta.glowacka263@gmail.com>
2024-04-13 22:53:26 +0200

    Init

--- include/sort.h
+++ include/sort.h
@@ -1,8 +1,2 @@
 #include <vector>
 
-std::vector<int> insertion_sort(std::vector<int> array); // Marta
-std::vector<int> shell_sort(std::vector<int> array); // Marta
-std::vector<int> selection_sort(std::vector<int> array); // Marta
-std::vector<int> heap_sort(std::vector<int> array); // Julia
-std::vector<int> quick_sort_left_pivot(std::vector<int> array); // Julia
-std::vector<int> quick_sort_random_pivot(std::vector<int> array); // Julia
